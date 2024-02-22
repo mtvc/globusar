3a9475b17715c67ae61539d3d39e0827a0a196d6
mtvc <matavac@gmail.com>
2024-02-22 16:49:10 +0100

    update commit

--- src/js/main.js
+++ src/js/main.js
@@ -1,98 +1,115 @@
+document.addEventListener('DOMContentLoaded', function () {
+  // Initialize the map
+  const map = L.map('map').setView([0, 0], 2);
 
-// Getting random longitude and latitude
+  // Add the tile layer
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    // attribution:
+    //'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  }).addTo(map);
 
-const btnSearch = document.querySelector('.btn-search-country');
-const countryContainer = document.querySelector('.country-container');
-const map = L.map('map');
+  const btnSearch = document.querySelector('.btn-search-country');
+  const countryContainer = document.querySelector('.country-container');
+  let marker;
 
-btnSearch.addEventListener('click', getCoords);
+  btnSearch.addEventListener('click', getCoords);
 
-function getCoords() {
-  const from = -90;
-  const to = 90;
-  const fromLng = -180;
-  const toLng = 180;
-  const fixed = 5;
+  function getCoords() {
+    const from = -90;
+    const to = 90;
+    const fromLng = -180;
+    const toLng = 180;
+    const fixed = 5;
 
-  const lat = (Math.random() * (to - from) + from).toFixed(fixed) * 1;
-  const lng = (Math.random() * (toLng - fromLng) + fromLng).toFixed(fixed) * 1;
+    const lat = (Math.random() * (to - from) + from).toFixed(fixed) * 1;
+    const lng =
+      (Math.random() * (toLng - fromLng) + fromLng).toFixed(fixed) * 1;
 
-  const coords = [lat, lng];
+    const coords = [lat, lng];
 
-  countryContainer.innerHTML = '';
+    countryContainer.innerHTML = '';
 
-  map.setView(coords, 2);
+    map.setView(coords, 2);
 
-  const renderCountry = function (data, className = '') {
-    const isIndependent =
-      data.independent === true ? 'Independent' : 'Not independent';
-    const html = `
-      <article class="country ${className}">
-              <img class="country__img" src="${data.flag}" />
-              <div class="country__data">
-                <h3 class="country__name">${data.name}</h3>
-                <h4 class="country__region">${data.region}</h4>
-                <p class="country__row"><span>👫 </span>${(
-                  +data.population / 1000000
-                ).toFixed(3)} M people</p>
-                <p class="country__row"><span>🗣️ </span>${
-                  data.languages[0].name
-                }</p>
-                <p class="country__row"><span>💰 </span>${
-                  data.currencies[0].name
-                }</p>
-                  <p class="country__row"><span>🚩 </span>${isIndependent}</p>
-              </div>
-            </article>
-            `;
+    // Remove previous marker if exists
+    if (marker) {
+      map.removeLayer(marker);
+    }
 
-    countryContainer.insertAdjacentHTML('beforeend', html);
-  };
+    const renderCountry = function (data, className = '') {
+      const isIndependent =
+        data.independent === true ? 'Independent' : 'Not independent';
+      const html = `
+        <article class="country ${className}">
+                <img class="country__img" src="${data.flag}" />
+                <div class="country__data">
+                  <h3 class="country__name">${data.name}</h3>
+                  <h4 class="country__region">${data.region}</h4>
+                  <p class="country__row"><span>👫 </span>${(
+                    +data.population / 1000000
+                  ).toFixed(3)} M people</p>
+                  <p class="country__row"><span>🗣️ </span>${
+                    data.languages[0].name
+                  }</p>
+                  <p class="country__row"><span>💰 </span>${
+                    data.currencies[0].name
+                  }</p>
+                    <p class="country__row"><span>🚩 </span>${isIndependent}</p>
+                </div>
+              </article>
+              `;
 
-  // Get country by coords
-  fetch(
-    `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}&localityLanguage=en`
-  )
-    .then(res => {
-      if (!res.ok) throw new Error(`Problem with geocoding`);
-      return res.json();
-    })
-    .then(data => {
-      console.log(data);
-      console.log(data.countryName);
+      countryContainer.insertAdjacentHTML('beforeend', html);
+    };
 
-      if (!data.countryName || data.countryName === 'Antarctica') {
-        getCoords();
+    // Get country by coords
+    fetch(
+      `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}&localityLanguage=en`
+    )
+      .then(res => {
+        if (!res.ok) throw new Error(`Problem with geocoding`);
+        return res.json();
+      })
+      .then(data => {
+        console.log(data);
+        console.log(data.countryName);
 
-        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-          // attribution:
-        //'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-        }).addTo(map);
-        return;
-      }
+        if (!data.countryName || data.countryName === 'Antarctica') {
+          getCoords();
 
-      const countryNameMapping = {
-        'United States of America (the)': 'usa',
-        'Russian Federation (the)': 'russia',
-        'Congo (the Democratic Republic of the)': 'congo',
-        'Sudan (the)': 'sudan',
-      };
+          L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            // attribution:
+            //'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+          }).addTo(map);
+          return;
+        }
 
-      if (countryNameMapping[data.countryName]) {
-        data.countryName = countryNameMapping[data.countryName];
-      }
+        const countryNameMapping = {
+          'United States of America (the)': 'usa',
+          'Russian Federation (the)': 'russia',
+          'Congo (the Democratic Republic of the)': 'congo',
+          'Sudan (the)': 'sudan',
+        };
 
-      // Show countries
-      L.marker(coords).addTo(map).bindPopup(`${lat}, ${lng}`).openPopup();
+        if (countryNameMapping[data.countryName]) {
+          data.countryName = countryNameMapping[data.countryName];
+        }
 
-      return fetch(`https://restcountries.com/v2/name/${data.countryName}`);
-    })
-    .then(res => {
-      // console.log(res.ok);
-      if (res.ok === 0) throw new Error(`Country not found!`);
-      return res.json();
-    })
+        marker = L.marker(coords)
+          .addTo(map)
+          .bindPopup(`${lat}, ${lng}`)
+          .openPopup();
 
-    .then(data => renderCountry(data[0]))
-    .catch(err => console.error(`${err.message}`));
-}
+        // Show countries
+        return fetch(`https://restcountries.com/v2/name/${data.countryName}`);
+      })
+      .then(res => {
+        // console.log(res.ok);
+        if (res.ok === 0) throw new Error(`Country not found!`);
+        return res.json();
+      })
+
+      .then(data => renderCountry(data[0]))
+      .catch(err => console.error(`${err.message}`));
+  }
+});
